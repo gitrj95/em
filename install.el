0dc1e6438d76eb553f20ab3562c3bb56b6dcdb35
rj <fwd@rj95.be>
2022-08-30 04:00:13 +0000

    pkt/list.txt
    - read package list from a file
    - specify file inside pkg root

--- install.el
+++ install.el
@@ -2,16 +2,22 @@
 ;;;; rj
 
 
+(defun load-pkg-list (root)
+  (let* ((file (concat root "list.txt"))
+	 (strings (with-temp-buffer
+		    (insert-file-contents file)
+		    (split-string (buffer-string) "\n" t))))
+    (mapcar #'intern strings)))
+
 (require 'package)
 (setq package-archives nil)
-(setq pkg-root
-      (concat default-directory "pkg/"))
+(setq pkg-root (concat default-directory "pkg/"))
 (defun calculate-package-roots (tail)
     (concat pkg-root tail))
 (add-to-list 'package-archives `("gnu" . ,(calculate-package-roots "gnu/")))
 (add-to-list 'package-archives `("nongnu" . ,(calculate-package-roots "nongnu/")))
-(setq package-selected-packages
-      '(vertico corfu-terminal orderless marginalia embark gtags-mode wgrep vundo))
+
+(setq package-selected-packages (load-pkg-list pkg-root))
 (package-install-selected-packages t)
 
 (setq em-etc-directory (file-truename "etc/"))
