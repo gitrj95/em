57cdc9edf5d808a04feeb651e6a8fc0d971d71c6
rj <fwd@rj95.be>
2024-01-06 09:54:09 -0600

    Revert "Change <f9> to eat. More likely to use this than eshell."
    This reverts commit dd410c77b18013a12d07c73196a3a80aeeb8225c.

--- init.el
+++ init.el
@@ -284,7 +284,7 @@
 (use-package eat
   ;; extant bug in `https://codeberg.org/akib/emacs-eat/issues/109'
   :bind
-  ("<f9>" . eat)
+  ("<f9>" . eshell)
   :hook
   (eshell-mode . eat-eshell-mode)
   (eshell-mode . eat-eshell-visual-command-mode))
